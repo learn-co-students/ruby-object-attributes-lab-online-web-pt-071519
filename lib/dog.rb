a8270fc91a741b64473930af1eee6898f771b151
Annie Truex <artruex@gmail.com>
2019-08-01 22:11:47 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog 
+  def name 
+    @name
+  end
+  def name=(dogs_name)
+    @name = dogs_name
+  end
+  def breed 
+    @breed 
+  end
+  def breed=(breed)
+    @breed = breed
+  end
+  
+end
