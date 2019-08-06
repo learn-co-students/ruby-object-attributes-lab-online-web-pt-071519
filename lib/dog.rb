615fcddecf14737b39db8bf3525f1904c68f85fb
Patrick Harrison <jahman@bellatlantic.net>
2019-08-06 18:07:25 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+  def name
+    @name
+  end
+  def name=(dogs_name)
+    @name = dogs_name
+  end
+  
+end
+ def name
+    @name
+  end
+  def name=(dogs_name)
+    @name = dogs_name
+  end
+  
+end
+ name = Dog.new
+ name.name = "Fido" 
